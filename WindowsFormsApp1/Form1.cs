259efec978fd134e2692c55846e95162265f5cc5
vedantgite <vedantgite7899@gmail.com>
2020-03-11 20:38:28 +0530

    Add files via upload

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form2 f1 = new Form2();
+            f1.Show();
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            textBox1.Focus();
+            
+
+        }
+        //this is the code for assigning shortcuts to elements (Alt+X, Alt+S etc etc):
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Modifiers==Keys.Control && e.KeyCode == Keys.F)
+            {
+                textBox2.Focus();
+            }
+        }
+        //But the above code doesnt seem to work.
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            if (textBox1.TextLength < 8)
+            {
+                int parsedValue;
+                if (!int.TryParse(textBox1.Text, out parsedValue) && textBox1.Text!="")
+                {
+                    button1.Enabled = false;
+                    textBox1.Text = "";
+                    MessageBox.Show("Error, Student number should only contain numeric values");
+                    button4.Enabled = false;
+                    button1.Enabled = false;
+                    button2.Enabled = false;
+                    button3.Enabled = false;
+
+
+                }
+                else
+                {
+                    button1.Enabled = true;
+                    button3.Enabled = true;
+                    button4.Enabled = true;
+                }
+            }
+            else if (textBox1.TextLength > 8)
+            {
+                button1.Enabled = false;
+                button4.Enabled = true;
+                button3.Enabled = false;
+            }
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            int parsedValue;
+            if (textBox2.TextLength >= 40)
+            {
+                MessageBox.Show("Error, Name should only be upto 40 characters");
+                textBox2.Text = "";
+                button4.Enabled = false;
+            }
+            else if (textBox2.TextLength < 40 && textBox2.TextLength > 0)
+            {
+                button4.Enabled = true;
+            }
+            if (int.TryParse(textBox2.Text, out parsedValue))
+            {
+                MessageBox.Show("Error, Name should only contain alphabets");
+                textBox2.Text = "";
+                button4.Enabled = false;
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            int parsedValue;
+            int parsedValue2;
+            bool res = Int32.TryParse(textBox3.Text, out parsedValue);
+            bool res2 = Int32.TryParse(textBox1.Text, out parsedValue2);
+            if (res2 == true && textBox3.Text!="")
+            {
+                if (res == true && parsedValue <= 100 && parsedValue >= 0)
+                {
+                    button2.Enabled = true;
+                }
+                else
+                {
+                    button2.Enabled = false;
+                    textBox3.Text = "";
+                    MessageBox.Show("Error, Enter grades between 0 to 100 only!");
+                }
+            }
+            else if(textBox3.Text != "")
+            {
+                button2.Enabled = false;
+                textBox3.Text = "";
+                MessageBox.Show("Error, Student number contains only numbers and it should not be empty!");
+                
+            }
+            if(textBox3.TextLength<=5)
+            {
+                button4.Enabled = true;
+            }
+            else
+            {
+                button4.Enabled = false;
+                MessageBox.Show("Error, Grades can only accept upto 5 characters");
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox1.Focus();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }      
+}
+
---
+++ WindowsFormsApp1/Form2.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Number of students:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Class Average:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(232, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(232, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "label4";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 187);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Location = new System.Drawing.Point(10, 10);
+            this.Name = "Form2";
+            this.Text = "ClassMonitorForm";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
